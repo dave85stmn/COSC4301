349de570ce53a36ac36b991951e8f1b900651165
David Mehoves <davida@mehoves.com>
2025-08-28 03:06:04 +0000

    Created the Hello world .cpp and .h files as well as proper header and comments

---
+++ Assignment1/main.cpp
@@ -0,0 +1,26 @@
+/* *************************************************
+*  Name: David Mehoves
+*  Assignment: Assignment 1: Hello World
+*  Purpose: To demonstrate the proper setup
+*   of the VM environment as well as two IDEs
+************************************************* */
+
+#include "main.h"
+#include <iostream>
+
+int main() {
+/* **********************************
+ * This function is the application driver. It demonstrates
+ *  a basic hello world example.
+ *
+ * @param na : na
+ * @return (int) : application exit code
+ * @exception na : na
+ * @note na
+ * **********************************/
+
+
+    std::cout << "hello world" << std::endl;;
+
+    return 0;
+}
---
+++ Assignment1/main.h
@@ -0,0 +1,18 @@
+/* *************************************************
+*  Name: David Mehoves
+*  Assignment: Assignment 1: Hello World
+*  Purpose: To demonstrate the proper setup
+*   of the VM environment as well as two IDEs
+************************************************* */
+
+
+#ifndef MAIN_H
+#define MAIN_H
+
+// Standard
+
+
+// Local
+
+
+#endif //MAIN_H

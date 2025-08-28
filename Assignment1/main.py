6e12aed982d7ca2eb3276e497392a7001880436e
David Mehoves <davida@mehoves.com>
2025-08-28 02:49:24 +0000

    Added proper header to the main.py file as well as a main() function and comments

--- Assignment1/main.py
+++ Assignment1/main.py
@@ -1,2 +1,24 @@
+# ###########################################
+# Name: David Mehoves
+# Assignment: Assignment 1: Hello World
+# Purpose: To demonstrate the proper setup
+#   of the VM environment as well as two IDEs
+# ###########################################
 
-print("Hello World")
+def main():
+    """
+    This function is the application driver. It provides
+        a basic hello world example and main file structure.
+
+    @param na : na
+
+    @return na
+
+    @exception na : na
+
+    @note na
+    """
+
+    print("Hello World")
+
+main()
